4f919d44e52c04be1b61b2e20b12f31750dc7909
Andriy Duyko <andriy@aduyko.com>
2013-09-09 14:25:59 -0400

    bullets now shoot, targeting reticle exists

--- game.js
+++ game.js
@@ -1,3 +1,4 @@
+//game objects
 var player = {
   x:250,
   y:250,
@@ -5,17 +6,43 @@ var player = {
   width:2,
   color:'white',
 }
+var mouseChaser = {
+  x:0,
+  y:0,
+  spawnDistance:15,
+  radius:3,
+  width:.5,
+  color:'white',
+  update: function(x,y){
+    angle=Math.atan((y-player.y)/(x-player.x));
+    pos=(x<=player.x?-1:1);
+    this.x=player.x+this.spawnDistance*Math.cos(angle)*pos;
+    this.y=player.y+this.spawnDistance*Math.sin(angle)*pos;
+  },
+}
 var bullets=[];
 var Bullet = function(x,y){
   this.angle=Math.atan((y-player.y)/(x-player.x));
   this.pos=(x<=player.x?-1:1);
-  console.log(this);
-  this.x=x;
-  this.y=y;
+  this.spawnDistance=15;
+  this.x=player.x+this.spawnDistance*Math.cos(this.angle)*this.pos;
+  this.y=player.y+this.spawnDistance*Math.sin(this.angle)*this.pos;
   this.radius=3;
   this.width=.5;
   this.color='red';
+  this.speed=5;
+  this.move=function(){
+    this.x+=this.speed*Math.cos(this.angle)*this.pos;
+    this.y+=this.speed*Math.sin(this.angle)*this.pos;
+  }
+  this.isDead=function(){
+    if (this.x+this.radius>canvas.width) return true;
+    if (this.x-this.radius<0) return true;
+    if (this.y+this.radius>canvas.height) return true;
+    if (this.y-this.radius<0) return true;
+  }
 }
+var enemies=[];
 var Enemy = function(initHp){
   this.x=Math.random()*canvas.width;
   this.y=Math.random()*canvas.height;
@@ -23,9 +50,15 @@ var Enemy = function(initHp){
   this.width=2;
   this.color='gray';
   this.speed=Math.random()*10+5;
+  this.move=function(){
+    this.x+=this.speed*Math.cos(this.angle);
+    this.y+=this.speed*math.sin(this.angle);
+  }
   this.hp=initHp;
 }
+//end game objects
 
+//init and run
 function init(){
   canvas = document.getElementById("canvas");
   ctx = canvas.getContext("2d");
@@ -35,7 +68,6 @@ function init(){
   },false);
   document.addEventListener('mouseup',function(e) {
     delete keysDown['mouse'];
-    bullets.push(new Bullet(mousePos.x,mousePos.y));
   },false);
   document.addEventListener('mousemove',function(e) {
     mousePos = getMouseCoords(e);
@@ -44,11 +76,13 @@ function init(){
 function run(){
   update();
   render();
+  //temporary dumb loop set to non-ensured 60fps
   setTimeout(function(){
     run();
     /*fps*/increaseCount();
   },16.7)
 }
+//end init and run
 
 /*fps stuff*/
 var count=0;
@@ -61,15 +95,29 @@ setInterval(function(){
 },1000);
 /*end fps stuff*/
 
+//logic
 function update(){
-  if('mouse' in keysDown){
-   // bullets.push(new Bullet(mousePos.x,mousePos.y));
+  mouseChaser.update(mousePos.x,mousePos.y);
+  for(var i=0;i<bullets.length;i++){
+    bullets[i].move();
+    if (bullets[i].isDead()){
+      bullets.splice(i,1);
+    }
   }
+  if('mouse' in keysDown){
+    bullets.push(new Bullet(mousePos.x,mousePos.y));
+  }
+
 }
+//end logic
+
+//rendering
 function render(){
   renderBackground();
   renderPlayer();
+  renderMouse();
   renderBullets();
+  renderEnemies();
 }
 function renderBackground(){
   ctx.fillStyle="black";
@@ -83,6 +131,14 @@ function renderPlayer(){
   ctx.stroke();
   ctx.closePath();
 }
+function renderMouse(){
+  ctx.beginPath();
+  ctx.arc(mouseChaser.x, mouseChaser.y, mouseChaser.radius, 0, 2*Math.PI, false);
+  ctx.lineWidth = mouseChaser.width;
+  ctx.strokeStyle = mouseChaser.color;
+  ctx.stroke();
+  ctx.closePath();
+}
 function renderBullets(){
   for (var i=0;i<bullets.length;i++){
     ctx.beginPath();
@@ -93,11 +149,23 @@ function renderBullets(){
     ctx.closePath();
   }
 }
+function renderEnemies(){
+
+}
+//end rendering
+
+//helper functions
+/**
+* returns key value pair for mouse coordinates
+* x and y offset are from the edge of the screen
+* y is 10 because our css margin is 10, x is auto so we do half canvas - half document
+* not sure if this is compatible on all browsers
+**/
 function getMouseCoords(event) {
   var x,y;
   x=(event.pageX);
   y=(event.pageY);
-  x-=432;
+  x+=250-document.width/2;
   y-=10;
   return {"x": x, "y": y};
 }
